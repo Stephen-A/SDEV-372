abdb96a327ac873e7cace142aa61bf6cada60abb
Stephen Allen <Allen.Stephen@student.greenriver.edu>
2023-01-04 16:12:29 -0800

    Changes the Point class to include a Z coordinate.

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -4,11 +4,13 @@ public class Point
 {
     private double x;
     private double y;
+    private double z;
 
-    public Point(double x, double y)
+    public Point(double x, double y, double z)
     {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double getX()
@@ -31,6 +33,16 @@ public class Point
         this.y = y;
     }
 
+    public double getZ()
+    {
+        return z;
+    }
+
+    public void setZ(double z)
+    {
+        this.z = z;
+    }
+
     @Override
     public boolean equals(Object o)
     {
@@ -40,7 +52,8 @@ public class Point
         Point point = (Point) o;
 
         if (Double.compare(point.x, x) != 0) return false;
-        return Double.compare(point.y, y) == 0;
+        if (Double.compare(point.y, y) != 0) return false;
+        return Double.compare(point.z, z) == 0;
     }
 
     @Override
@@ -52,6 +65,8 @@ public class Point
         result = (int) (temp ^ (temp >>> 32));
         temp = Double.doubleToLongBits(y);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(z);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
 
@@ -61,7 +76,7 @@ public class Point
         return "Point{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
                 '}';
     }
-
 }
