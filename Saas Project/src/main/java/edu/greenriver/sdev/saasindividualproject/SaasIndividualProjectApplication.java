00a9033dccf64a43a91fe9a90051385c4a119b9f
Stephen Allen <Allen.Stephen@student.greenriver.edu>
2023-01-05 17:20:45 -0800

    Committing initial project files

---
+++ Saas Project/src/main/java/edu/greenriver/sdev/saasindividualproject/SaasIndividualProjectApplication.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.saasindividualproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasIndividualProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SaasIndividualProjectApplication.class, args);
+    }
+
+}
